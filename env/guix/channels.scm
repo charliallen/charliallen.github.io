fadf435d7093ae5a7dd331cb347a9cb16833d35a
Charli Allen <chaallen@redhat.com>
2026-09-10 02:10:31 -0400

    chore: fix guile module calls for guix

--- env/guix/channels.scm
+++ env/guix/channels.scm
@@ -1,3 +1,5 @@
+(use-modules (guix channels))
+
 (list (channel
        (name 'guix)
        (url "https://git.guix.gnu.org/guix.git")
--- env/guix/guix.scm
+++ env/guix/guix.scm
@@ -1,3 +1,4 @@
+(define-module (guix))
 (use-modules (packages charliallen))
 
 ;; Development entry point.  Invoke with `-L env/guix`.
